3d5b07be19c3256548141062d08541273188405a
Jiri Kuncar <jiri.kuncar@cern.ch>
2014-10-06 22:29:38 +0200

    File storage tests
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- tests/test_collect.py
+++ tests/test_collect.py
@@ -1,9 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Flask-Collect.
+# Copyright (C) 2012, 2013, 2014 Kirill Klenov.
+# Copyright (C) 2014 CERN.
+#
+# Flask-Collect is free software; you can redistribute it and/or modify it
+# under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
 from flask import Flask, Blueprint
 from flask_collect import Collect
 from functools import partial
 from os import path as op
+from shutil import rmtree
 from tempfile import mkdtemp
 from unittest import TestCase
+
+
+def filter_(order, items):
+    """Filter application blueprints."""
+    def _key(item):
+        if item.name in order:
+            return order.index(item.name)
+        return -1
+    return sorted(items, key=_key)
 
 
 class BaseTest(TestCase):
@@ -36,6 +56,7 @@ class BaseTest(TestCase):
         self.assertEqual(len(test), 3)
 
     def test_filter(self):
+        """Test blueprint filter."""
         app = Flask(__name__)
 
         blueprint = Blueprint('test1', __name__, static_folder='static1')
@@ -45,13 +66,6 @@ class BaseTest(TestCase):
         app.register_blueprint(blueprint)
 
         static_root = mkdtemp()
-
-        def filter_(order, items):
-            def _key(item):
-                if item.name in order:
-                    return order.index(item.name)
-                return -1
-            return sorted(items, key=_key)
 
         app.config['COLLECT_STATIC_ROOT'] = static_root
         app.config['COLLECT_FILTER'] = partial(filter_, ['test3', 'test1'])
@@ -66,3 +80,25 @@ class BaseTest(TestCase):
         collect = Collect(app)
         test = list(collect.collect(verbose=True))
         self.assertTrue('static1' in test[1][1])
+
+    def test_file_storage(self):
+        """Test file storage."""
+        app = Flask(__name__)
+
+        blueprint = Blueprint('test1', __name__, static_folder='static1')
+        app.register_blueprint(blueprint)
+
+        blueprint = Blueprint('test3', __name__, static_folder='static3')
+        app.register_blueprint(blueprint)
+
+        static_root = mkdtemp()
+
+        app.config['COLLECT_STATIC_ROOT'] = static_root
+        app.config['COLLECT_FILTER'] = partial(filter_, ['test3', 'test1'])
+        app.config['COLLECT_STORAGE'] = 'flask.ext.collect.storage.file'
+
+        collect = Collect(app)
+        collect.collect()
+
+        with open(op.join(static_root, 'test.css'), 'r') as file_:
+            self.assertTrue('body { color: red; }' in file_.read())
